ad1759e7fd39a60f9e6882badc86125e25678959
swpen <48854524+swpen@users.noreply.github.com>
2019-03-25 10:20:52 +1100

    testing first update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+<h1>UI Design files</h1>
+<div>  
+  <a href="https://codesandbox.io/s/github/swpen/pensolds" target="_blank" rel="noopener noreferrer">Pentana Design System Github Repo</a>.
 </div>
 `;
