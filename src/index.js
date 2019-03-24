d7d8c42e59669f755905d181a4a8cbb43424cb7a
swpen <48854524+swpen@users.noreply.github.com>
2019-03-25 10:26:38 +1100

    test update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>UI Design files</h1>
+<h1>Pentana Solutions Design System</h1>
 <div>  
   <a href="https://codesandbox.io/s/github/swpen/pensolds" target="_blank" rel="noopener noreferrer">Pentana Design System Github Repo</a>.
 </div>
+<br /><hr /><br />
+<p>paragraph</p>
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+<button>Button</button>
+<p></p>
+<button class="primary">Button</button>
+<button class="primary" disabled>Button</button>
+<button class="primary">Loading...</button>
+<button class="primary">&#128465</button>
+<button class="primary">&#128465;Delete</button>
+<p></p>
+<button class="secondary">Button</button>
+<button class="secondary" disabled>Button</button>
+<button class="secondary">Loading...</button>
+<button class="secondary">&#128465</button>
+<button class="secondary">&#128465;Delete</button>
+<p></p>
+Breadcrumb > Breadcrumb > Breadcrumb > Breadcrumb
+<p></p>
 `;
