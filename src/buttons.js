b248061ec14226481c26dad4b87fd52454bb30b9
swpen <48854524+swpen@users.noreply.github.com>
2019-03-25 11:54:06 +1100

    Buttons added

---
+++ src/buttons.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Buttons</h1>
+<button>Button</button>
+<p></p>
+<button class="primary">Button</button>
+<button class="primary" disabled>Button</button>
+<button class="primary">Loading...</button>
+<button class="primary">&#128465</button>
+<button class="primary">&#128465;Delete</button>
+<p></p>
+<button class="secondary">Button</button>
+<button class="secondary" disabled>Button</button>
+<button class="secondary">Loading...</button>
+<button class="secondary">&#128465</button>
+<button class="secondary">&#128465;Delete</button>
+<p></p>
+`;
