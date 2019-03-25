a8eaf3f1036e0565e1946d472db89afa76ca56fa
swpen <48854524+swpen@users.noreply.github.com>
2019-03-26 09:36:39 +1100

    update

--- src/buttons.js
+++ src/buttons.js
@@ -4,17 +4,18 @@ document.getElementById("app").innerHTML = `
 <h1>Buttons</h1>
 <hr>
 <button>Button</button>
-<p></p>
+<br><br>
 <button class="primary">Button</button>
 <button class="primary" disabled>Button</button>
 <button class="primary">Loading...</button>
 <button class="primary">&#128465</button>
 <button class="primary">&#128465;Delete</button>
-<p></p>
+<br><br>
 <button class="secondary">Button</button>
 <button class="secondary" disabled>Button</button>
 <button class="secondary">Loading...</button>
 <button class="secondary">&#128465</button>
 <button class="secondary">&#128465;Delete</button>
-<p></p>
+<br><br>
+<button class="secondary">Split Button</button>
 `;
--- src/typography.js
+++ src/typography.js
@@ -10,5 +10,12 @@ document.getElementById("app").innerHTML = `
 <h4>Heading 4</h4>
 <h5>Heading 5</h5>
 <h6>Heading 6</h6>
-<small>small</small>
+<small>Use small to create lighter, secondary text.</small>
+<p>Use the mark element to <mark>highlight</mark> text.</p>
+<p>A <abbr title="Dealership Management System">DMS</abbr> is a bundled management information system created specifically for automotive industry car dealerships.</p>
+<blockquote>
+  <p>Pentana Solutions provides a number of different Dealer Management Systems (DMS) to Australian and New Zealand retail dealerships.</p>
+  <footer>Pentana Solutions</footer>
+</blockquote>
+<p>The following HTML elements: <code class="w3-codespan">span</code>, <code class="w3-codespan">section</code>, and <code class="w3-codespan">div</code> defines a section in a document.</p>
 `;
