e589e3092beae747a4c3627ea06a023c0f7c7689
swpen <48854524+swpen@users.noreply.github.com>
2019-03-27 15:27:16 +1100

    update

--- src/typography.js
+++ src/typography.js
@@ -13,6 +13,7 @@ document.getElementById("app").innerHTML = `
 <small>Use small to create lighter, secondary text.</small>
 <p>Use the mark element to <mark>highlight</mark> text.</p>
 <p>A <abbr title="Dealership Management System">DMS</abbr> is a bundled management information system created specifically for automotive industry car dealerships.</p>
+<p>Use <kbd>ctrl + p</kbd> to open the Print dialog box.</p>
 <blockquote>
   <p>Pentana Solutions provides a number of different Dealer Management Systems (DMS) to Australian and New Zealand retail dealerships.</p>
   <footer>Pentana Solutions</footer>
